0e9c96394dce8c2b91fd19bd2f9b54687473c9a5
checkiamsiam <issiam02415@gmail.com>
2022-09-12 22:50:49 +0600

    banner complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import Button from "../Button/Button";
 const Header = () => {
   return (
     <div className="fixed top-0 w-full">
-      <div className="container mx-auto">
+      <div className="lg:px-32 md:px-20 sm:px-10 px-5">
         <div className="navbar bg-transparent">
           <div className="navbar-start">
             <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <label tabIndex={0} className="xl:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                 </svg>
               </label>
@@ -36,14 +36,14 @@ const Header = () => {
             </div>
             <a className="normal-case text-xl font-bold"><span className="text-primary">Doc</span><span className="text-secondary">mic</span></a>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <div className="flex text-lg justify-center items-center gap-5 text-neutral">
-              <a className="text-primary" href="">Home</a>
-              <a href="">About</a>
-              <a href="">Departments</a>
-              <a href="">Pages</a>
-              <a href="">Blogs</a>
-              <a href="">Contacts</a>
+          <div className="navbar-center hidden xl:flex">
+            <div className="flex lg:text-lg justify-center items-center gap-5 text-neutral">
+              <a className="text-primary transition duration-500" href="">Home</a>
+              <a className="hover:text-primary transition duration-500" href="">About</a>
+              <a className="hover:text-primary transition duration-500" href="">Departments</a>
+              <a className="hover:text-primary transition duration-500" href="">Pages</a>
+              <a className="hover:text-primary transition duration-500" href="">Blogs</a>
+              <a className="hover:text-primary transition duration-500" href="">Contacts</a>
             </div>
           </div>
           <div className="navbar-end ">
--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -2,25 +2,27 @@ import React from "react";
 import bannerImage from "../../../assets/pose_2.png";
 import Button from "../../../components/Button/Button";
 import checkupButton from '../../../assets/Frame-7.png'
+import doctors from '../../../assets/Group3.png'
+import ornament from '../../../assets/Ornament19.png'
 
 const Banner = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex justify-center items-center lg:pt-0 pt-20 ">
       <div>
-        <div className="container mx-auto">
+        <div className="lg:px-32 md:px-20 sm:px-10 px-5">
           <div className="lg:flex gap-10">
             <div className="lg:w-1/2 mb-2 pt-2 flex justify-center items-center">
               <div className="">
-                <p className="lg:text-7xl sm:text-5xl text-3xl font-semibold text-accent">
+                <p className="lg:text-6xl sm:text-5xl text-3xl font-semibold text-accent">
                   Your <span className="text-primary">Health</span> Is Our Top <span className="text-secondary">Priority</span>
                 </p>
-                <p className="sm:text-xl sm:mt-8 text-lg text-neutral ">
+                <p className="sm:text-xl sm:mt-8 mt-3 text-lg text-neutral ">
                   There are many variations of passages of lpsum available, but the majority hae suffered.
                 </p>
-                <div className="sm:mt-8 ">
+                <div className="sm:mt-8 mt-3">
                   <Button>Meet Our Specialist</Button>
                 </div>
-                <div className="sm:mt-10 ">
+                <div className="sm:mt-10 mt-3">
                   <div className="flex justify-between">
                     <div>
                       <h1 className="text-center font-semibold text-primary text-2xl">262k+</h1>
@@ -39,12 +41,23 @@ const Banner = () => {
               </div>
             </div>
            
-            <div className="lg:w-1/2 flex justify-center items-center relative">
+            <div className="lg:w-1/2 flex justify-center items-center  lg:pt-0 pt-5">
+              <div className="relative">
               <div className="bg-ellipseFive bg-cover bg-center bg-no-repeat">
                 <img src={bannerImage} alt="Img" height="auto" className="" />
               </div>
-              <div className="absolute left-0 bottom-36 cursor-pointer">
+              <div className="sm:absolute left-0 bottom-32 cursor-pointer">
                 <img src={checkupButton} alt="" />
+              </div>
+              <div className="p-3 bg-base-100 sm:mt-0 mt-4 rounded-lg sm:absolute bottom-0 right-0 z-20">
+                  <h1 className="mb-3 font-semibold">Meet Our Doctors</h1>
+                  <img src={doctors} alt="" />
+                  <div className="w-full h-3 my-1 bg-gradient-to-r from-primary to-white rounded-lg"></div>
+                  <div className="w-full h-3 my-1 bg-gradient-to-r from-primary to-white rounded-lg"></div>
+              </div>
+              <div className="sm:absolute lg:block hidden -bottom-16 right-24 z-10">
+                  <img src={ornament} alt="" />
+              </div>
               </div>
             </div>
           </div>
